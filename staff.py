d060cf55579c15b2bf643da532be24161f2d256f
hsiuyineez <hsiuyinwong@gmail.com>
2025-02-15 20:11:47 +0800

    Create staff.py

---
+++ staff.py
@@ -0,0 +1,9 @@
+class Staff:
+    def __init__(self,staff_id):
+        self.__staff_id=staff_id
+
+    def get_staff_id(self):
+        return self.__staff_id
+
+    def set_staff_id(self,staff_id):
+        self.__staff_id=staff_id
